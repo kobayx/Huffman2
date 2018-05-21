9ebec23669118a4e3e48b90dd0ff59f26b7e3ebb
kobayx <38417130+kobayx@users.noreply.github.com>
2018-05-20 19:53:42 -0700

    Update huff.cpp

--- Huffman/huff.cpp
+++ Huffman/huff.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <cmath>
 #include "PQHuffman.h"
 #include "bitStreams.h"
 using namespace std;
@@ -19,31 +20,36 @@ void dump(map<unsigned char, int> count) {
 		cerr << i << "|" << setw(9) << right << dec << itm.second << endl;
 	}
 }
-void makeHuff(Heap<item<unsigned char, int>*, int>& Q) {
-	item<item<unsigned char, int>*, int>* Qu = new item<item<unsigned char, int>*, int>(nullptr, 0);
+void makeHuff(Heap<item<unsigned char, int>*, int>& PQheap) {
 	item<unsigned char, int>* Qu2;
 	item<unsigned char, int>* Qu3;
 
-	while (Q.heap.size() > 1) {
+	while (PQheap.heap.size() > 1) {
 		item<unsigned char, int>* Qu4 = new item<unsigned char, int>();
-		Qu2 = Q.pop()->data();
-		Qu3 = Q.pop()->data();
+		Qu2 = PQheap.pop()->data();
+		Qu3 = PQheap.pop()->data();
 		Qu4->right() = Qu3;
 		Qu4->left() = Qu2;
 		Qu4->priority() = Qu2->priority() + Qu3->priority();
-		Q.push(Qu4, Qu4->priority());
+		PQheap.push(Qu4, Qu4->priority());
 	}
 }
 
 int main() {
-	bitStreams decypher("words.txt.puff");
+	bitStreams decypher("words.txt.puff", true);
 	map<unsigned char, int> count;
-	Heap<item<unsigned char, int>*, int> Q;
+	pair<unsigned char, unsigned long long> CStable[256];
+	Heap<item<unsigned char, int>*, int> PQheap;
 	unsigned char words = '\0';
 	int ct = 0;
 	int cnt = 0;
 	ofstream fout;
 	fstream fin;
+	unsigned long long pattern;
+	unsigned char bitcount;
+	unsigned char getter;
+	unsigned char byte = 0;
+
 	///////////////////////////////////////
 	// HUFF
 	//////////////////////////////////////
@@ -58,60 +64,75 @@ int main() {
 	fin.close();
 	for (auto& itm : count) {
 		item<unsigned char, int>* Que = new item<unsigned char, int>(itm.first, itm.second);
-		Q.push(Que, itm.second);
+		PQheap.push(Que, itm.second);
 	}
 //	dump(count);
-	makeHuff(Q);
+	makeHuff(PQheap);
 //	Q.dump(Q.heap[0]->data(), 10);
-	Q.buildCStable(Q.heap[0]->data());
+	PQheap.buildCStable(PQheap.heap[0]->data());
 
 	fin.open("words.txt", fstream::in);
 	if (fin.fail())
 		cerr << "fail" << endl;
-	for (auto& itm : Q.symbolz) {
-		unsigned char bitcount = itm.first;
-		unsigned char pattern = cnt;
-		Q.bitsBytes_().putByte(bitcount);
-		if (itm.first != 0) {
-			Q.encode(pattern);
-			Q.bitsBytes_().flush();
+	for (auto itm : PQheap.symbols) {
+		bitcount = itm.first;
+		getter = cnt;
+		PQheap.bitsBytes_().putByte(bitcount);
+		if (bitcount != 0) {
+			PQheap.encode(getter);
+			PQheap.bitsBytes_().flush();
 		}
 		cnt++;
 	}
+	
 	cnt = 0;
 	while(cnt < ct) {
 		unsigned char temp = fin.get();
-		Q.encode(temp);
+		PQheap.encode(temp);
 		cnt++;
 	}
 	fin.close();
-	Q.bitsBytes_().closeFile();
+	PQheap.bitsBytes_().closeFile();
 	///////////////////////////////////////
 	//PUFF
 	///////////////////////////////////////
-	fin.open("output.txt.huff", fstream::in | fstream::binary);
+ 	fin.open("output.txt.huff", fstream::in | fstream::binary);
 	if (fin.fail())
 		cerr << "fail" << endl;
 	cnt = 0;
-	unsigned char bitcount;
-	unsigned char bytes;
-	int repeat = 0;
+	while (cnt < 256) {
+		bitcount = fin.get();
+//		if (bitcount > 0) {
+		unsigned char bytes = ceil(bitcount / 8.);
+		byte = bytes;
+		for (unsigned char j = 0; j < bytes; ++j) {
+			if (j != 0)
+				decypher.bufferOut() <<= 8;
+			getter = fin.get();
+			decypher.bufferOut() |= getter;
+		}
+		decypher.bufferOut() >>= ((byte * 8) - bitcount);
+		CStable[cnt] = make_pair(bitcount, decypher.bufferOut());
+		decypher.bufferOut() = '\0';
+		cnt++;
+	}
+	cnt = 0;
+	for (auto T : CStable) {
+		unsigned char data_;
+		data_ = cnt;
+		bitcount = T.first;
+		pattern = T.second;
+		if(pattern == 940)
+			cout << "stop" << endl;
+		if(bitcount != 0)
+			decypher.rebuild(pattern, data_, bitcount);
+		++cnt;
+	}
+
+	decypher.root();
 	while (!fin.eof()) {
-		if (cnt < 256) {
-			bitcount = fin.get();
-			if (bitcount != 0) {
-				repeat = ceil(bitcount/8.);
-				while (repeat > 0) {
-					decypher.rebuild(fin.get());
-					--repeat;
-				}
-			}
-			++cnt;
-		}
-		else {
-			++cnt;
-			fin.get();
-		}
+		getter = fin.get();
+		decypher.decode(getter);
 	}
 	return 0;
 }
